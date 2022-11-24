fa4ebec0bec8d4c758654fa3471d3659b4774c0c
Vainillas <maliberti@unrn.edu.ar>
2022-11-24 13:20:50 -0300

    toma gil

--- app/Controllers/FixtureController.php
+++ app/Controllers/FixtureController.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use App\Models\GroupModel;
 use App\Models\MatchModel;
 use App\Models\PhaseModel;
+use App\Models\TournamentModel;
 
 class FixtureController extends BaseController
 {
@@ -13,6 +14,7 @@ class FixtureController extends BaseController
         $modelMatch = model(MatchModel::class);
         $modelPhase = model(PhaseModel::class);
         $modelGroup = model(GroupModel::class);
+        $modelTournament = model(TournamentModel::class);
         $phases = $modelPhase->getPhasesByTournamentIdOrderByStartDate($id_tournament);
 
         foreach ($phases as $p) {
@@ -22,7 +24,7 @@ class FixtureController extends BaseController
         }
 
         $data = [
-            'id_tournament' => $id_tournament,
+            'id_tournament' => $modelTournament->getTournaments($id_tournament),
             'phases' => $modelPhase->getPhasesByTournamentIdOrderByStartDate($id_tournament),
             'phase_groups' => $phase_groups,
             'groups' => $modelGroup->getGroupsByIdTournament($id_tournament),
