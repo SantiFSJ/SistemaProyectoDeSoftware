f696809af8deafa9ca4aadbe71e6b97f3f9c2e0c
SantiFSJ <santiagofernandezsanjuan@gmail.com>
2023-11-28 17:49:58 -0300

    fix de consulta

--- app/Models/ForecastModel.php
+++ app/Models/ForecastModel.php
@@ -25,9 +25,8 @@ class ForecastModel extends Model
         return $this->where(['id_bet' => $id])->delete();
     }
     public function findByUserAndPhase($id_user, $id_phase){
-        $query = $this->db->query('SELECT m.*, s.name as stadium_name, tl.name as local, tv.name visitor, (select b.id from bets b where b.id_user = ' . $id_user . ' and b.id_phase = ' . $id_phase . ') bet_id, f.id as forecast_id, f.expected_result
-        FROM matches m left join teams tl on ( m.id_local = tl.id) join teams tv on (m.id_visitor = tv.id) left join forecasts f ON (f.id_match = m.id) join stadiums s ON (m.id_stadium = s.id) where m.id_phase = ' . $id_phase . ' order by 1');
-
+        $query = $this->db->query('SELECT m.*, s.name as stadium_name, tl.name as local, tv.name visitor, b.id as bet_id, f.id as forecast_id, f.expected_result FROM matches m left join teams tl on ( m.id_local = tl.id) join teams tv on (m.id_visitor = tv.id) left outer join bets b ON (b.id_user =  ' . $id_user . ' and b.id_phase = ' . $id_phase . ') left join forecasts f ON (f.id_bet = b.id AND f.id_match = m.id)  join stadiums s ON (m.id_stadium = s.id) where m.id_phase = ' . $id_phase . ' order by 1');
         return $query->getResult();
     }
+
 }
