c3ffc3548a6c7e111919ad81dcba8ca02c1505c8
Vainillas <maliberti@unrn.edu.ar>
2023-12-12 21:17:39 -0300

    format invites

--- app/Models/InvitesModel.php
+++ app/Models/InvitesModel.php
@@ -10,7 +10,8 @@ class InvitesModel extends Model
     protected $primaryKey = 'id';
     protected $allowedFields = ['id_challenge', 'id_user_invited', 'response'];
 
-    public function getInvites($id = false){
+    public function getInvites($id = false)
+    {
         $builder = $this->db->table($this->table . ' i');
         if ($id === false) {
             $builder->select('i.id, c.id, c.name as challenge_name, t.id, t.name as tournament_name, u.id, u.username')
@@ -27,7 +28,8 @@ class InvitesModel extends Model
         return $builder->get()->getResult();
     }
 
-    public function getInvitesByUser($id_user){
+    public function getInvitesByUser($id_user)
+    {
         $builder = $this->db->table($this->table . ' i');
         $builder->select('i.id, i.response, c.id, c.name as challenge_name, t.id, t.name as tournament_name, u.id as user_id, u.username')
             ->join('challenges c', 'c.id = i.id_challenge', 'inner')
@@ -37,7 +39,8 @@ class InvitesModel extends Model
         return $builder->get()->getResult();
     }
 
-    public function getPendingsInvitesByUser($id_user){
+    public function getPendingsInvitesByUser($id_user)
+    {
         $builder = $this->db->table($this->table . ' i');
         $builder->select('i.id, i.response, c.id as challenge_id, c.name as challenge_name, t.id as tournament_id, t.name as tournament_name, u.id as user_id, u.username')
             ->join('challenges c', 'c.id = i.id_challenge', 'inner')
@@ -47,7 +50,8 @@ class InvitesModel extends Model
         return $builder->get()->getResult();
     }
 
-    public function getAcceptedInvitesByUser($id_user){
+    public function getAcceptedInvitesByUser($id_user)
+    {
         $builder = $this->db->table($this->table . ' i');
         $builder->select('i.id, c.id, c.name as challenge_name, t.id, t.name as tournament_name, u.id, u.username')
             ->join('challenges c', 'c.id = i.id_challenge', 'inner')
@@ -57,7 +61,8 @@ class InvitesModel extends Model
         return $builder->get()->getResult();
     }
 
-    public function getRejectedInvitesByUser($id_user){
+    public function getRejectedInvitesByUser($id_user)
+    {
         $builder = $this->db->table($this->table . ' i');
         $builder->select('i.id, c.id, c.name as challenge_name, t.id, t.name as tournament_name, u.id, u.username')
             ->join('challenges c', 'c.id = i.id_challenge', 'inner')
@@ -67,7 +72,8 @@ class InvitesModel extends Model
         return $builder->get()->getResult();
     }
 
-    public function rejectInvite($id){
+    public function rejectInvite($id)
+    {
         $builder = $this->db->table($this->table);
         $data = [
             'response' => 'REJECTED'
@@ -75,7 +81,8 @@ class InvitesModel extends Model
         $builder->where('id', $id)->update($data);
     }
 
-    public function acceptInvite($id){
+    public function acceptInvite($id)
+    {
         $builder = $this->db->table($this->table);
         $data = [
             'response' => 'ACCEPTED'
